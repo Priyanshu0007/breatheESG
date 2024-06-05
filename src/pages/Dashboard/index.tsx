b5db922616de18c3761ff72db25ee8943e7b73a5
Priyanshu0007 <priyanshu2859@gmail.com>
2024-06-06 00:38:26 +0530

    navbar completed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import "./style.scss"
+import { MdBarChart,MdGridView,MdOutlinePieChartOutline,MdOutlineInsertDriveFile,MdOutlineSupervisedUserCircle,MdAutoGraph, MdLogout } from "react-icons/md";
+import { FiLayers } from "react-icons/fi";
+import { PiTarget } from "react-icons/pi";
+import { useFirebase } from "../../context/firebase";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+
 
 
 const Dashboard = () => {
+  const navItems=[
+    {id:0,name:"Dashboard",icon:<MdBarChart/>,class:""},
+    {id:1,name:"Entity Manager",icon:<MdGridView/>,class:""},
+    {id:2,name:"Data Manager",icon:<MdOutlinePieChartOutline/>,class:"selected"},
+    {id:3,name:"Reporting",icon:<MdOutlineInsertDriveFile/>,class:""},
+    {id:4,name:"Materiality",icon:<FiLayers/>,class:""},
+    {id:5,name:"Suppliers",icon:<MdOutlineSupervisedUserCircle/>,class:""},
+    {id:6,name:"Analytics",icon:<MdAutoGraph/>,class:""},
+    {id:7,name:"Targets",icon:<PiTarget/>,class:""},
+  ]
+  const firebase:any = useFirebase();
+  const nagivate=useNavigate();
+  useEffect(()=>{
+    if(!firebase.user?.email){nagivate('/sign')}
+    else{nagivate('/')}
+  },[firebase.user, nagivate])
   return (
-    <div>Dashboard</div>
+    <div className="d-main">
+      <div className="sidebar">
+        <div><img src="/src/assets/logo.png"/></div>
+        <div>
+            {navItems.map((it)=>{
+              return(<div className={`nav-items ${it.class}`} key={it.id}><p>{it.icon}</p><p>{it.name}</p></div>)
+            })}
+            <div onClick={()=>{firebase.signOutUser();toast.success('Succesfull Logged Out',{duration:3000})}} className={`nav-items logout`} ><p><MdLogout/></p><p>Logout</p></div>
+        </div>
+      </div>
+      <div className="d-content">
+        
+      </div>
+    </div>
   )
 }
 
